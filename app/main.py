2347294f8c7375786c370acb7a04134d66ef8292
klabruben3 <adept.ruben@gmail.com>
2026-08-06 00:30:11 +0200

    CORS update

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://academiq-nwu.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
